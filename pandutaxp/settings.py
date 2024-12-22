1c4182305a0f9a000475c9a938d3c40b559a00d8
SuzanFah <susiefahim@gmail.com>
2024-12-22 06:30:23 +0200

    Add Gmail SMTP configuration with successful email test. Features: - Configure email backend with secure App Password - Test email functionality returning success (1) - Enable automated email capabilities for verification and notifications

--- pandutaxp/settings.py
+++ pandutaxp/settings.py
@@ -118,3 +118,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'client_dashboard'  # For clients
 # or
 LOGIN_REDIRECT_URL = 'provider_dashboard'  # For providers
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'susiefahim@gmail.com'
+EMAIL_HOST_PASSWORD = 'bmla lnnh abct maji'
